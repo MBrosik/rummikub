80b13a9a9c33e4e52283fdd3d5cb015876f95c62
MBrosss <49322957+MBrosss@users.noreply.github.com>
2021-05-15 23:24:05 +0200

    server

---
+++ server/src/main/kotlin/app.kt
@@ -0,0 +1,28 @@
+import spark.Request
+import spark.Response
+import spark.Spark
+import spark.Spark.get
+
+fun main() {
+    println("Hello world");
+
+    // ---------------
+    // server config
+    // ---------------
+    Spark.port(5000)
+    Spark.staticFileLocation("/public")
+
+    Spark.before({ request, response ->
+        response.header("Access-Control-Allow-Methods", "*")
+        response.header("Access-Control-Allow-Origin", "*")
+    })
+
+    get("/") { request, response -> mainPageGet(request, response) }
+
+}
+
+
+fun mainPageGet(request:Request, response:Response):String{
+    response.type("application/json");
+    return "{'a':1}";
+}
